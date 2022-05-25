438e36a5faf3211bcf22c088f643bf35e9760247
chisato1000310 <chisato.kuwada@studerande.karlskogafhs.se>
2022-05-25 11:22:23 +0200

    Add slider

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    //   },
+})
